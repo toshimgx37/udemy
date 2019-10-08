a187a508457684238d7d9fa289760782850e7eb4
toshimgx37 <[toshi.112537@i.softbank.jp]>
2019-10-09 06:35:50 +0900

    CircleButton changed

--- App.js
+++ App.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import { StyleSheet, View } from 'react-native';
 import Appbar from "./src/components/Appbar";
-import MemoListScreen from "./src/screens/MemoListScreen";
+import MemoDetailScreen from "./src/screens/MemoDetailScreen";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Appbar />
-      <MemoListScreen />
-    </View>
-  );
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Appbar />
+        <MemoDetailScreen />
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
